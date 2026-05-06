354baa4a7e329a0b89b81db3d8259849ba4df5fd
Zurrpel <jendoc.bezrukoff@yandex.ru>
2026-05-06 16:50:26 +0400

    Внедрение фичи F-0001

--- ProcessView/Domain/ProcessInfo.cs
+++ ProcessView/Domain/ProcessInfo.cs
@@ -5,5 +5,12 @@ public sealed class ProcessInfo
     public int ProcessId { get; init; }
     public int ParentProcessId { get; init; }
     public string Name { get; init; } = string.Empty;
+
+    public int ThreadCount { get; init; }
+    public int UsageCount { get; init; }
+    public int ModuleId { get; init; }
+    public long DefaultHeapId { get; init; }
+    public int PriorityClassBase { get; init; }
+    public int Flags { get; init; }
 }
 
